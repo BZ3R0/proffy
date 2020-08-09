5086763cf2f2f950210701ca2db4cc68fb3175f4
Caio Rocha <caio.tecnoativa>
2020-08-09 17:19:51 -0300

    retriving teacher information from api and styling whatsapp link

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -1,35 +1,54 @@
-import React from 'react';
+import React, { ReactHTML } from 'react';
 
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
 import './styles.css';
+import api from '../../services/api';
 
-function TeacherItem() {
+export interface Teacher {
+    id: number;
+    avatar: string;
+    bio: string;
+    cost: number;
+    name: string;
+    subject: string;
+    whatsapp: string;
+}
+
+interface TeacherItemProps {
+    teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+    function createNewConnection() {
+        api.post('connections', {
+            user_id: teacher.id,
+        });
+    }
+
     return (
         <article className="teacher-item">
             <header>
-                <img src="https://media-exp1.licdn.com/dms/image/C4D03AQG3nzT2n0og6g/profile-displayphoto-shrink_200_200/0?e=1602115200&v=beta&t=FY8XEsrrl2B-gnhXB5qxZkfkqCXuoctocLW0_1QwPHo" alt="Caio Broflovski"/>
+                <img src={teacher.avatar} alt={teacher.name} />
                 <div>
-                    <strong>Caio Broflovski</strong>
-                    <span>Química</span>
+                    <strong>{teacher.name}</strong>
+                    <span>{teacher.subject}</span>
                 </div>
             </header>
 
             <p>
-                Apaixonado pelo E.C Bahia.
-                <br/><br/>
-                Desde que comprou sua primeira camisa do Bahia, nunca mais conseguiu tirar, e viaja para todos os lugares com a mesma. Que já sabe até andar sozinha.
+                {teacher.bio}
             </p>
 
             <footer>
                 <p>
                     Preço/Hora
-                    <strong>R$ 80,00</strong>
+                    <strong>R$ {teacher.cost}</strong>
                 </p>
-                <button type="button">
+                <a onClick={createNewConnection} href={`https://wa.me/${teacher.whatsapp}`} target="_blank">
                     <img src={whatsappIcon} alt="Whatsapp"/>
                     Entrar em contato
-                </button>
+                </a>
             </footer>
         </article>
     )
